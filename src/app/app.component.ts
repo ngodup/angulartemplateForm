817ca7140a13d4572c292f5df1fe213c34e4ff21
ngodup <ngodupbz@gmail.com>
2021-12-05 19:36:19 +0530

    custom form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'formTemplate';
+  user: any;
+  student = { name: '', phone: '', email: '' };
+
+  ngOnInit() {
+    this.user = {
+      name: '',
+      email: '',
+      address: {
+        location: '',
+        country: ''
+      }
+    };
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhoneValidatorDirective } from './validators/phone-validator.directive';
+import { EmailValidatorDirective } from './validators/email.validator.';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhoneValidatorDirective,
+    EmailValidatorDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
